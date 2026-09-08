a04553c9a36cbdda6ad6a461346ba289b1b9d72a
Bimal Pathak <bimalpathak6667@gmail.com>
2026-09-08 08:17:53 +0545

    week 2 day 1 update

--- week2/script.js
+++ week2/script.js
@@ -1,3 +1,9 @@
 function sum() {
     document.getElementById("num").innerText = Number(document.getElementById("first").value) + Number(document.getElementById("second").value);
 }
+function diff() {
+    document.getElementById("num").innerText = Number(document.getElementById("first").value) - Number(document.getElementById("second").value);
+}
+function mult() {
+    document.getElementById("num").innerText = Number(document.getElementById("first").value) * Number(document.getElementById("second").value);
+}
