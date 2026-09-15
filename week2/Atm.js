df711111eca051e90038cd3a9ab905756beb7b40
Pathak_ofc <bimalpathak6667@gmail.com>
2026-09-15 09:56:05 +0545

    a

--- week2/Atm.js
+++ week2/Atm.js
@@ -3,12 +3,10 @@ let balance = 1000;
 let balanceTxt = document.getElementById("balance");
 balanceTxt.innerText = " Balance = ******";
 let pin = "1234";
-
 function toggleBalance() {
     blc = !blc;
     balanceTxt.innerText = blc ? "Balance = " + balance : "Balance = ******";
 }
-
 function withDraw() {
     let money = Number(prompt(`enter amount to withdraw  
                             ** it should be multiple of 100 **`));
@@ -32,7 +30,6 @@ function withDraw() {
         }
     }
 }
-
 function deposit() {
     let money = Number(prompt("Enter amunt to deposit"));
     if (money % 100 == 0) {
